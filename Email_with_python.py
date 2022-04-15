193985efa62e660c223309660a69bde4dd51807c
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 17:53:51 +0530

    Update Email_with_python.py

--- Email_with_python.py
+++ Email_with_python.py
@@ -1,29 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import smtplib  #inbuilt library in python
+ #importing inbuilt library in python.
+  
+import smtplib 
 from email.message import EmailMessage
 
-
-# In[13]:
-
-
 msg=EmailMessage()
-msg["Subject"]="Testing is being done"
-msg["From"]="Shubham"
-msg["To"]='sgharde14@gmail.com'
-msg.set_content("Hello this mail is sent by using python")
+msg["Subject"]="Testing is being done"   #subject which we have to give.
+msg["From"]="Shubham"                    #name of the sender
+msg["To"]='sgharde14@gmail.com'          #here we mention the receivers email id.
+msg.set_content("Hello this mail is sent by using python")       #the content we want to send.
 
-server=smtplib.SMTP_SSL("smtp.gmail.com",465)
-server.login("ghardes72@gmail.com","Shubham@12")
+server=smtplib.SMTP_SSL("smtp.gmail.com",465)           #establishing a connection with goggle gmail.
+server.login("ghardes72@gmail.com","Shubham@12")         #here we mention the email id of the sender.
 server.send_message(msg)
-server.quit()
+server.quit()                                           #here we close the connection.
 
 
-# In[ ]:
 
 
 
