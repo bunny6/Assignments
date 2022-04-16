abde8856ffb8d08a096580eafec5f2067d8d680c
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 15:53:58 +0530

    Update MultiThreading_Task.py

--- Multithreading/MultiThreading_Task.py
+++ Multithreading/MultiThreading_Task.py
@@ -1,38 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#importing libraries.
 import openpyxl
 
-
-# In[17]:
-
-
+#importing the dataset.
 path="multi_threading_activity.xlsx"
-
-
-# In[18]:
-
 
 obj = openpyxl.load_workbook(path)
 
-
-# In[20]:
-
-
 sheet1 = obj.active
 
-
-# In[21]:
-
-
 mat=[]
-
-
-# In[25]:
-
 
 for i in range(1, sheet1.max_row+1):
   row=[]
@@ -41,28 +17,11 @@ for i in range(1, sheet1.max_row+1):
     row.append(cell_obj.value)
   mat.append(row)
  
-
-
-# In[102]:
-
-
 mat.pop()
 
-
-# In[103]:
-
-
 mat.pop()
-
-
-# In[105]:
-
 
 new=mat[1:]
-
-
-# In[106]:
-
 
 for i in range(len(new)):
   for j in range(2):
@@ -73,24 +32,11 @@ for i in range(len(new)):
       print(new[i][j])
       new[i][j]=0
 
-
 print(new)      
-
-
-# In[107]:
-
 
 print(len(new))
 
-
-# In[195]:
-
-
 import threading
-
-
-# In[209]:
-
 
 result=[]
 def sum_of_two_rows(new):
@@ -100,10 +46,6 @@ def sum_of_two_rows(new):
   result.append(sumMat) 
   print(sumMat)
 
-
-# In[210]:
-
-
 def diff_of_tow_rows(new):
   diffMat=[]
   for i in range(len(new)):
@@ -111,63 +53,23 @@ def diff_of_tow_rows(new):
   result.append(diffMat)
   print(diffMat)
 
-
-# In[211]:
-
-
 t1=threading.Thread(target=sum_of_two_rows,args=(new,))
-
-
-# In[212]:
-
 
 t2=threading.Thread(target=diff_of_tow_rows,args=(new,))
 
-
-# In[213]:
-
-
 t1.start()
-
-
-# In[214]:
-
 
 t2.start()
 
-
-# In[215]:
-
-
 a=t1.join()
-
-
-# In[216]:
-
 
 b=t2.join()
 
-
-# In[217]:
-
-
 print(result)
-
-
-# In[220]:
-
 
 col1=result[0]
 
-
-# In[223]:
-
-
 col1
-
-
-# In[222]:
-
 
 col2=result[1]
 
