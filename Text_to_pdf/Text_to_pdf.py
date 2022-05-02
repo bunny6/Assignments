baaee8f2ee067be74950fba0da9982fbdeb9f999
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-05-02 20:08:25 +0530

    Update Text_to_pdf.py

--- Text_to_pdf/Text_to_pdf.py
+++ Text_to_pdf/Text_to_pdf.py
@@ -2,24 +2,26 @@ import PyPDF2
 
 pdfFileObj = open('sample.pdf', 'rb')
 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  #given a pdf here as argument
 
-n=pdfReader.numPages
+n=pdfReader.numPages  #number of pages in pdf
 
 print(n)
 
-textfile= open('textfile.txt','w') 
+textfile= open('textfile.txt','w') #created a new text file to write contents from pdf after reading
 
+#used for loop to iterrate over all the pages of pdf
 for i in range(n):
-    content=pdfReader.getPage(i).extractText()
+    content=pdfReader.getPage(i).extractText()   #read content from each page
     print(content)
     textfile.write(content)
-    textfile.write('\n')
+    textfile.write('\n')   #written into text file
     
-file=open('textfile.txt','r')
+file=open('textfile.txt','r')    # to open text file
 
 data = file.read()
 
+#function to search whether particular word in text file or not
 def searching_Word(sw):
     if sw in data:
         occurrences = data.count(sw)
@@ -31,7 +33,7 @@ def searching_Word(sw):
     
   
 sw=input("Enter the word to search")
-print(searching_Word(sw))
+print(searching_Word(sw))  #it will print the number of times a word repeated in file
 
 
 
