f7b13cae6ed831e0b82c45df39dd1e64aec7492b
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-06-01 19:03:05 +0530

    Update Price_prediction.py

--- Price_prediction.py
+++ Price_prediction.py
@@ -1,88 +1,83 @@
 #importing libraries
+import math
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow as tf
-
-#installing tensorflow
-pip install tensorflow
-
-#reading the dataset
-df=pd.read_csv("trainingset.csv")
-
-df.head()
-
-#checking for null values
-df.isnull().sum()
-
-df.info()
-
-#dropped unimportant columns
-df=df.drop(['id'],axis=1)
-
-df=df.drop(['date'],axis=1)
-
-df
-
-plt.figure(figsize=(12,12))
-sns.heatmap(df.corr(),annot=True,linecolor ='black', linewidths = 1)
-
-X=df.iloc[:,1:].values
-Y=df.iloc[:,0].values
-
-print(X)
-
-print(Y)
-
-Y = Y.reshape((17290, -1))
-
-print(Y)
-
-Y
-
-X
-
+import matplotlib.pyplot as plt
+from tensorflow.keras import Model
+from tensorflow.keras import Sequential
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import Dense, Dropout
 from sklearn.model_selection import train_test_split
-X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=1)
+from tensorflow.keras.losses import MeanSquaredLogarithmicError
 
-#scaling the data
+#imporing the datasets.
+train_data = pd.read_csv('trainingset.csv')
+test_data = pd.read_csv('testset.csv')
+
+train_data.head()
+
+test_data.head()
+
+#checking for null values.
+train_data.isnull().sum()
+
+test_data.isnull().sum()
+
+#ploting the heatmap for checking the correlation between the target column and other features
+import seaborn as sns
+plt.figure(figsize=(14,14))
+sns.heatmap(train_data.corr(),annot=True,linecolor ='black', linewidths = 1)
+
+plt.figure(figsize=(14,14))
+sns.heatmap(test_data.corr(),annot=True,linecolor ='black', linewidths = 1)
+
+#droping unwanted columns
+train_data=train_data.drop(['id','date','zipcode','lat','long','yr_renovated','sqft_lot','condition','yr_built','sqft_lot15'],axis=1)
+test_data=test_data.drop(['id','date','zipcode','lat','long','yr_renovated','sqft_lot','condition','yr_built','sqft_lot15'],axis=1)
+
+#spliting the dataset
+X_train=train_data.iloc[:,1:].values
+Y_train=train_data.iloc[:,0].values
+
+X_test=test_data.iloc[:,1:].values
+Y_test=test_data.iloc[:,0].values
+
+Y_train = Y_train.reshape((17290, -1))
+Y_test = Y_test.reshape((4323, -1))
+
+#scaling the dataset.
 from sklearn.preprocessing import StandardScaler
 sc_X=StandardScaler()
 sc_y=StandardScaler()
 X_train=sc_X.fit_transform(X_train)
 Y_train=sc_y.fit_transform(Y_train)
+X_test=sc_X.fit_transform(X_test)
+Y_test=sc_y.fit_transform(Y_test)
 
-print(Y_train)
-
-#Ann install
+#importing the ann.
 ann = tf.keras.models.Sequential()
 
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+#adding  3 hidden layers
+ann.add(tf.keras.layers.Dense(units=160, activation='relu'))
 
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=480, activation='relu'))
 
-ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+ann.add(tf.keras.layers.Dense(units=160, activation='relu'))
+ 
+#adding output layer
+ann.add(tf.keras.layers.Dense(units=1, activation='linear'))
 
-ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+#loss function
+msle=MeanSquaredLogarithmicError()
 
-#training
-ann.fit(X_train, Y_train, batch_size = 32, epochs = 100)
+ann.compile(optimizer = 'adam', loss = 'msle', metrics = ['msle'])
+
+#training the ann
+ANN=ann.fit(X_train, Y_train, batch_size = 64, epochs = 35, validation_split=0.2)
 
 #predicting
-y_pred = ann.predict(X_test)
-
-print(y_pred)
-
-df1=Y_test.copy()
-
-df1
-
-print(y_pred)
-
-print(np.concatenate((y_pred.reshape(len(y_pred),1), Y_test.reshape(len(Y_test),1)),1))
-
-
+Y_test['prediction'] = ann.predict(Y_test)
 
 
 
